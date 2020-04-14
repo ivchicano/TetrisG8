d8aae383f64119811bc08f8d6f7fa7c3955302a3
Albertocalib <Albertocanal_1998@hotmail.es>
2020-04-14 21:45:39 +0200

    Delete unnecesary things

--- app/src/test/java/com/example/tetrisg8/TetrisBDDCucumberClass.java
+++ app/src/test/java/com/example/tetrisg8/TetrisBDDCucumberClass.java
@@ -1,8 +1,5 @@
 package com.example.tetrisg8;
 
-import androidx.test.rule.ActivityTestRule;
-
-import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
@@ -12,7 +9,5 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features = "src/test/java/com/example/tetrisg8/scenarios/")
 public class TetrisBDDCucumberClass {
 
-    @Rule
-    ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
 }
